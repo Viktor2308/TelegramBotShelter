db1922c41459147c742cf9039d76471572cf1460
Viktor <viktor_23_08@mail.ru>
2023-04-06 15:56:08 +0200

    initial commit

---
+++ src/test/java/com/example/telegrambotshelter/TelegramBotShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotShelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
